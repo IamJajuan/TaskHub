109e53974b06d05b9a3f2dd6b57b1b87565024db
Jajuan Hall <jajuanleehall1998@gmail.com>
2021-01-22 13:20:30 -0500

    Create header

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types';
+import { AppBar, Toolbar,Typography,Button } from '@material-ui/core';
+import { HeaderStyles } from '../styles/Header';
+import { NavLink } from 'react-router-dom';
+
+
+const Header = ({isAuth}) => {
+
+     const classes = HeaderStyles()
+    return (
+        <header>
+            <AppBar position = "static">
+<Toolbar  className = {classes.nav} >
+    <Typography variant = "h6">
+        Taskhub
+    </Typography>
+    <div>
+       {isAuth ? <Fragment>
+           <NavLink to = "/" color = "inherit" component = {Button}>
+               Home
+           </NavLink>
+        <Button color = "inherit" >Logout </Button>
+       </Fragment> : <Fragment></Fragment>}
+
+    </div>
+</Toolbar>
+            </AppBar>
+        </header>
+    )
+}
+
+Header.propTypes = {
+
+    isAuth:PropTypes.bool.isRequired
+}
+
+export default Header
