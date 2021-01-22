6b89003c37eb0423d110c4f9b91662964b358872
Jajuan Hall <jajuanleehall1998@gmail.com>
2021-01-22 13:20:30 -0500

    Create header

---
+++ src/styles/Header.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+
+    nav: {
+        display:'flex',
+        justifyContent:"space-between"
+
+        
+      },
+}))
+
+export const HeaderStyles = useStyles
