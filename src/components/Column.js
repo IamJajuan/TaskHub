173b7788072c5845d9c2e2ff9e7210bc1b1998df
Jajuan Hall <jajuanleehall1998@gmail.com>
2021-01-26 18:54:36 -0500

    Add task form

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 import PropTypes from 'prop-types'
 import ColumnHeader from './ColumnHeader'
 import { Box, Button } from '@material-ui/core'
 import { Add } from '@material-ui/icons'
 import TaskCard from './TaskCard'
-const Column = ({name,id,tasks,toggleModal,open}) => {
+import TaskForm from './TaskForm'
+
+const Column = ({name,id,tasks}) => {
+       
+    const [openTask, setOpenTask] = useState(false)
+    const toggleTaskModal = () => setOpenTask(prev => !prev)
+
+    const addTask = (vals,close) => {
+        console.log(vals)
+        close()
+    }
+
     return (
         <Box width = "280px" marginRight= ".5em" >
-            <ColumnHeader toggleModal = {toggleModal} openForm = {open} id = {id} name = {name} />
-            <Button variant = "outlined" size = "small" fullWidth startIcon = {<Add/>}> Add Task </Button>
-            {tasks && tasks.map(item => <TaskCard {...item} />)}
+            <TaskForm  name = "" id = "" submit = {addTask} toggleModal = {toggleTaskModal} open={openTask} description = "" cost = "" title ="Add" stage = "" priority = ""   />
+            <ColumnHeader  id = {id} name = {name} />
+            <Button style = {{marginBottom:'1em'}} onClick = {toggleTaskModal} variant = "outlined" size = "small" fullWidth startIcon = {<Add/>}> Add Task </Button>
+           
+                {(tasks && tasks.map(item => (<TaskCard key = {item.id} {...item} />)  ))}
+
+
         </Box>
     )
 }
@@ -17,9 +32,7 @@ const Column = ({name,id,tasks,toggleModal,open}) => {
 Column.propTypes = {
 name:PropTypes.string.isRequired,
 id:PropTypes.string.isRequired,
-tasks:PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
-open:PropTypes.bool.isRequired,
-toggleModal:PropTypes.func.isRequired,
+tasks:PropTypes.arrayOf(PropTypes.object.isRequired),
 }
 
 export default Column
