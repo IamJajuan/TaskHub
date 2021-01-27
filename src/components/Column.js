813984dd183901df6f10901bfb8749082d7cefc3
Jajuan Hall <jajuanleehall1998@gmail.com>
2021-01-26 22:20:22 -0500

    Update card

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import PropTypes from 'prop-types'
 import ColumnHeader from './ColumnHeader'
-import { Box, Button } from '@material-ui/core'
+import { Box, Button,Card,CardContent } from '@material-ui/core'
 import { Add } from '@material-ui/icons'
 import TaskCard from './TaskCard'
 import TaskForm from './TaskForm'
@@ -17,12 +17,19 @@ const Column = ({name,id,tasks}) => {
     }
 
     return (
-        <Box width = "280px" marginRight= ".5em" >
-            <TaskForm  name = "" id = "" submit = {addTask} toggleModal = {toggleTaskModal} open={openTask} description = "" cost = "" title ="Add" stage = "" priority = ""   />
+        <Box width = "280px"  marginRight= ".5em" >
+           <Card>
+<CardContent>
+<TaskForm  name = "" id = "" submit = {addTask} toggleModal = {toggleTaskModal} open={openTask} description = "" cost = "" title ="Add" stage = "" priority = ""   />
             <ColumnHeader  id = {id} name = {name} />
-            <Button style = {{marginBottom:'1em'}} onClick = {toggleTaskModal} variant = "outlined" size = "small" fullWidth startIcon = {<Add/>}> Add Task </Button>
+            <Button style = {{marginBottom:'1em',marginTop:'1em'}} onClick = {toggleTaskModal} variant = "outlined" size = "small" fullWidth startIcon = {<Add/>}> Add Task </Button>
            
+                <Box>
                 {(tasks && tasks.map(item => (<TaskCard key = {item.id} {...item} />)  ))}
+                </Box>
+
+</CardContent>
+           </Card>
 
 
         </Box>
