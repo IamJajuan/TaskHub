d11f015fca997a82e52b5c2dc8b3ca281d5302d5
Jajuan Hall <jajuanleehall1998@gmail.com>
2021-01-29 00:31:07 -0500

    Add update column feature

--- src/actions/columns/ColumnActions.js
+++ src/actions/columns/ColumnActions.js
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import {ADDCOLUMN, REMOVECOLUMNS} from './ColumnTypes'
+import {ADDCOLUMN, REMOVECOLUMNS, UPDATECOLUMN} from './ColumnTypes'
 
 /**
  * Add the given column object to the reducer, then close modal when added successfully with the given function
@@ -14,7 +14,7 @@ export const addColumn = (col,func) => dispatch => {
 }
 
 /**
- *Remove the given columns from reducers with the given field with the given id, then close modal when removed successfully with the given function
+ *Remove the given columns from reducer with the given field with the given id, then close modal when removed successfully with the given function
 
  *@param {String} id the given id
  *@param {String} field the given field
@@ -27,3 +27,17 @@ export const removeColumns = (id,field,func) => dispatch => {
     dispatch({type:REMOVECOLUMNS,payload:{id,field}})
     func()
 }
+
+/**
+ * Update column from reducer with the given data, then close modal when updated successfully with the given function
+ * @param data the given data
+ * @param func the given function
+ */
+ 
+ export const updateColumn = (data,func) => dispatch => {
+
+    dispatch({type:UPDATECOLUMN,payload:data})
+    func()
+
+
+ }
