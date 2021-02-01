a5b33ac6d91912c0cb2d6a8b0221bdbdf1606171
Jajuan Hall <jajuanleehall1998@gmail.com>
2021-02-01 01:17:52 -0500

    update urls

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,7 @@ import { Box, Button, Container, Typography } from '@material-ui/core'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { GlobalStyles } from '../styles/Global'
-
+import {BASEURL} from '../CONSTANTS'
 const NotFound = () => {
 
     const classes = GlobalStyles()
@@ -16,7 +16,7 @@ const NotFound = () => {
             <Typography variant = "h3">
 Sorry, Page Not Found
             </Typography>
-            <Button className = {classes.btn} to = "/" variant = "contained" color = "primary" component = {Link} >
+            <Button className = {classes.btn} to = {BASEURL} variant = "contained" color = "primary" component = {Link} >
                 Go Back Home
             </Button>
         </Box>
